6712499b1720e2b30e3330ed180f9b53a9d4e386
Alexandra Labusova <sasa.labusova@gmail.com>
2023-01-28 14:57:30 +0100

    eslint fix

--- src/pages/play/Question.jsx
+++ src/pages/play/Question.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Question.css";
 import close from "../../assets/closeWhite.svg";
